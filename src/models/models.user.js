da02a057a2e9eb106b850ea679180ef0ca6a7706
suman kumar bhartiya <skb08725@gmail.com>
2024-08-27 10:05:24 +0530

    fix module not found error

--- src/models/models.user.js
+++ src/models/models.user.js
@@ -3,7 +3,7 @@ import { Model } from "sequelize";
 export default async (sequelize, DataTypes) => {
     class User extends Model {
         static associate(models) {
-            Users.hasMany(models.post, { foreignKey: 'id' });
+            User.hasMany(models.Post, { foreignKey: 'user_id' });
         }
     };
     User.init({
